a397692614ca64da5e96fe2764260dd105410e46
Michael Staal-Olsen <miso@netcompany.com>
2022-02-02 10:35:33 +0100

    Re-enter message input

--- nhs_reverification_hello_world/lambda_function.py
+++ nhs_reverification_hello_world/lambda_function.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello World - Reverification",
+            "message": "Hello World - Reverification PoC",
         }),
     }
