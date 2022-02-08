af26c9a0f03e4dedd008e3da4c052c1db0c49cd3
misonetcompany <69500742+misonetcompany@users.noreply.github.com>
2022-02-08 14:47:34 +0100

    Reformat files

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,11 +1,11 @@
-variable "AWS_REGION" {    
-    default = "us-east-1"
+variable "AWS_REGION" {
+  default = "us-east-1"
 }
 variable "AMI" {
-    type = "map"
-    
-    default {
-        eu-west-2 = "ami-03dea29b0216a1e03"
-        us-east-1 = "ami-0c2a1acae6667e438"
-    }
+  type = map(string)
+
+  default {
+    eu-west-2 = "ami-03dea29b0216a1e03"
+    us-east-1 = "ami-0c2a1acae6667e438"
+  }
 }
