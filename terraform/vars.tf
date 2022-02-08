c061ccc2a84e527b80b6788954a877b8c843668d
misonetcompany <69500742+misonetcompany@users.noreply.github.com>
2022-02-08 14:44:30 +0100

    Retry Terraform config

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,4 +1,11 @@
-variable "region" {}
-variable "main_vpc_cidr" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
+variable "AWS_REGION" {    
+    default = "us-east-1"
+}
+variable "AMI" {
+    type = "map"
+    
+    default {
+        eu-west-2 = "ami-03dea29b0216a1e03"
+        us-east-1 = "ami-0c2a1acae6667e438"
+    }
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,52 @@
+resource "aws_vpc" "prod-vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = "true" #gives you an internal domain name
+    enable_dns_hostnames = "true" #gives you an internal host name
+    enable_classiclink = "false"
+    instance_tenancy = "default"
+    
+    tags {
+        Name = "prod-vpc"
+    }
+}
+
+resource "aws_subnet" "prod-subnet-public-1" {
+    vpc_id = "${aws_vpc.prod-vpc.id}"
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = "true" //it makes this a public subnet
+    availability_zone = "eu-west-2a"
+    tags {
+        Name = "prod-subnet-public-1"
+    }
+}
+
+resource "aws_instance" "web1" {
+    ami = "${lookup(var.AMI, var.AWS_REGION)}"
+    instance_type = "t2.micro"
+    # VPC
+    subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+    # Security Group
+    vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
+    # the Public SSH key
+    key_name = "${aws_key_pair.london-region-key-pair.id}"
+    # nginx installation
+    provisioner "file" {
+        source = "nginx.sh"
+        destination = "/tmp/nginx.sh"
+    }
+    provisioner "remote-exec" {
+        inline = [
+             "chmod +x /tmp/nginx.sh",
+             "sudo /tmp/nginx.sh"
+        ]
+    }
+    connection {
+        user = "${var.EC2_USER}"
+        private_key = "${file("${var.PRIVATE_KEY_PATH}")}"
+    }
+}
+// Sends your public key to the instance
+resource "aws_key_pair" "london-region-key-pair" {
+    key_name = "london-region-key-pair"
+    public_key = "${file(var.PUBLIC_KEY_PATH)}"
+}
